06c63ef3c0728c181e5fe0dca74ebc907e4de06b
Aaaya26 <a.spring.spring5@gmail.com>
2024-01-16 11:28:54 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     validates :nickname,         presence: true
-    validates :family_name,      presence: true
-    validates :first_name,       presence: true
-    validates :family_name_kana, presence: true
-    validates :first_name_kana,  presence: true
+    validates :family_name,      presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+    validates :first_name,       presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+    validates :family_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ヶー－]+\z/ }
+    validates :first_name_kana,  presence: true, format: { with: /\A[ぁ-んァ-ヶー－]+\z/ }
     validates :birthday,         presence: true
+    
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i },on: :create
 end
