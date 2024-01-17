4e6b0ed37e75168ab872b39e14dc16b12d0d81c3
Aaaya26 <a.spring.spring5@gmail.com>
2024-01-17 21:15:57 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+  private
+
+  def message_params
+    params.require(:item).permit(:item_name, :item_data, :item_category_id, :item_status_id, :charge_id, :prefecture_id, :ship_date_id, :price :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  validates :item_name, presence: true
+  validates :item_data, presence: true
+  validates :price,     presence: true
+
 end
