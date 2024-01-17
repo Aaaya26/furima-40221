0451bdb88b7e73fa968e1fc7c56094704c66a500
Aaaya26 <a.spring.spring5@gmail.com>
2024-01-17 15:48:01 +0900

    パスワードについて異常系テストの細分化

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,8 +45,18 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
-      it 'passwordは半角英数字混合でなければ登録できない' do
+      it 'passwordは半角英字のみでは登録できない' do
         @user.password = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it 'passwordは半角数字のみでは登録できない' do
+        @user.password = '000000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it 'passwordは全角文字を含むものは登録できない' do
+        @user.password = 'Ａaa111'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is invalid")
       end
