d0592dcb09db541e8ce3efcc755d47f5d12aa690
Aaaya26 <a.spring.spring5@gmail.com>
2024-01-17 22:26:15 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,5 +11,9 @@ class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :item_data, presence: true
   validates :price,     presence: true
-  
+  validates :item_category_id    , numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_status_id      , numericality: { other_than: 1, message: "can't be blank" }
+  validates :charge_id           , numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id       , numericality: { other_than: 1, message: "can't be blank" }
+  validates :ship_date_id        , numericality: { other_than: 1, message: "can't be blank" }
 end
