386bc986394d020bf9015b9ab24711980aff44ad
Aaaya26 <a.spring.spring5@gmail.com>
2024-01-17 22:25:12 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,15 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_status
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :ship_date
 
   validates :item_name, presence: true
   validates :item_data, presence: true
   validates :price,     presence: true
-
+  
 end
