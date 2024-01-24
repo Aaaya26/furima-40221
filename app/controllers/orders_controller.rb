882eb0a6504707ad43f8e9f45ae40a6589098a0e
Aaaya26 <a.spring.spring5@gmail.com>
2024-01-24 23:20:52 +0900

    他関連機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,13 @@
 class OrdersController < ApplicationController
-before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
   def index
-    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-    @order_address = OrderAddress.new
+    if @item.order == nil
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      @order_address = OrderAddress.new
+    else
+      redirect_to root_path
+    end
   end
 
   def create
